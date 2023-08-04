0fceade423a8501040e8ba8292f832355841f7fe
Seungjin Lee <dltmdwls154@gmail.com>
2023-08-05 01:42:33 +0900

    1Day 1commit

--- src/styles/GlobalStyleStyled.tsx
+++ src/styles/GlobalStyleStyled.tsx
@@ -8,33 +8,7 @@ const GlobalStyleStyled = () => {
 
 export default GlobalStyleStyled
 
-//     --background: #161616;
-//     --background-card: #252525;
-//     --background-level-one: rgba(53,53,53,.5);
-//     --border-card: hsla(0,0%,100%,.1);
-//     --side-spacing: clamp(1rem,5vw,4em);
-//     --text-color-100: #eee;
-//     --text-color-70: #9b9b9b;
-//     --text-color-40: #5d5d5d;
-//     --font-size: 1rem;
-//     --blur: blur(30px);
-//     --blur-light: blur(15px);
-//     --color-positive: #26f52e;
-//     --border-size: 1px;
-//     --radius-big: 10rem;
-//     --radius-norm: 1rem;
-//     --transition-long: 800ms;
-//     --transition-medium: 400ms;
-//     --transition-short: 200ms;
-//     --curve: cubic-bezier(0.16,1,0.3,1);
-//     --card-inner-padding-vertical: 1.5rem;
-//     --card-inner-padding-horizontal: 2.25rem;
-//     --button-inner-padding-vertical: 1.25rem;
-//     --button-inner-padding-horizontal: 3rem;
-
 const GlobalStyle = createGlobalStyle`
-  
-  
   @font-face {
     font-family: 'JalnanTtf'; 
     src: local('JalnanTtf'), local('JalnanTtf');
